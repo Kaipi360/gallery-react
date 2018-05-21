77ce2f3505e766b1034c0b3a0d968a0dcef1d06e
Kai Victor <hkaivictor@gmail.com>
2018-05-21 15:42:31 +0200

    :sparkles: flickr public search working and react app added

--- backend-gallery/middeware/index.js
+++ backend-gallery/middeware/index.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { get } from 'axios';
+import Flickr from 'flickr-sdk';
 import logger from '../utils/logger';
 
 const app = Router();
@@ -8,13 +10,29 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/api/kai', (req, res) => {
-  logger.debug('api');
-  res.send(
-    JSON.stringify({
-      kai: 'kai'
-    })
-  );
+app.get('/api/search', async (req, res) => {
+  try {
+    const flickr = new Flickr('549da579245b015f6b47395bfff7833e');
+    const results = await flickr.photos.search({
+      text: 'doggo'
+    });
+    const formatedBody = {
+      ...results.body,
+      photos: {
+        ...results.body.photos,
+        photo: results.body.photos.photo.map(photo => ({
+          ...photo,
+          uri: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${
+            photo.id
+          }_${photo.secret}.jpg`
+        }))
+      }
+    };
+    res.end(JSON.stringify(formatedBody));
+  } catch (e) {
+    logger.error(e);
+  }
+  res.send(JSON.stringify(token));
 });
 
 export default app;
